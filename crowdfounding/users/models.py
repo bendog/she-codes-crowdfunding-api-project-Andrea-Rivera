a0106f7e8f50d3ce9d95ba7e3995e2d43a4c24ee
Andrea <andita54@hotmail.com>
2023-08-08 18:27:05 +1000

    updating models

--- crowdfounding/users/models.py
+++ crowdfounding/users/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class CustomUser(AbstractUser):
-    pass
+    business_description = models.TextField(null=True)
 
 def __str__(self):
     return self.username
+
